3ae525c45e616cec4a791225f9a230d9d1a3c820
Matthew <krashstudios@gmail.com>
2018-07-20 21:40:48 -0700

    contact page
    google maps not quite working with custom theme

--- js/centerGoogleMap.js
+++ js/centerGoogleMap.js
@@ -1,13 +1,15 @@
 var mapOptions = {
-zoom: 2,
-center: new google.maps.LatLng(0,0), mapTypeId: google.maps.MapTypeId.ROADMAP
+  zoom: 2,
+  center: new google.maps.LatLng(33.651066,-112.254376),
+  mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 
 var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
 // this is our gem
-google.maps.event.addDomListener(window, "resize", function() {
-var center = map.getCenter();
-google.maps.event.trigger(map, "resize");
-map.setCenter(center);
+google.maps.event.addDomListener(window, "resize",
+function() {
+  var center = map.getCenter();
+  google.maps.event.trigger(map, "resize");
+  map.setCenter(center);
 });
