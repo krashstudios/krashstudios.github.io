1ab823dffee391c27c49a74796a0deb847d03742
Matthew <krashstudios@gmail.com>
2018-10-28 23:17:19 -0700

    javascript class
    chapter 7 case project

--- js/calculatetime.js
+++ js/calculatetime.js
@@ -31,7 +31,7 @@ function getDateDifference(startDate, endDate) {
   var endMonth = endDate.getMonth();
   var endDay = endDate.getDate();
 
-  // We calculate February based on end year as it might be a leep year which might influence the number of days.
+  // february might be a leap year
   var february = (endYear % 4 == 0 && endYear % 100 != 0) || endYear % 400 == 0 ? 29 : 28;
   var daysOfMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
