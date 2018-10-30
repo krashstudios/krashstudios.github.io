8f7475be9a12aec2ad5c92d8572b4e65f4df43c2
Matthew <krashstudios@gmail.com>
2018-10-29 23:25:46 -0700

    chapter 7 case
    javascript class project

--- js/calculatetime.js
+++ js/calculatetime.js
@@ -19,10 +19,12 @@ function calculate() {
 }
 
 function getDateDifference(startDate, endDate) {
+
   if (startDate > endDate) {
     console.error('Start date must be before end date');
     return null;
   }
+
   var startYear = startDate.getFullYear();
   var startMonth = startDate.getMonth();
   var startDay = startDate.getDate();
@@ -33,14 +35,15 @@ function getDateDifference(startDate, endDate) {
 
   // february might be a leap year
   var february = (endYear % 4 == 0 && endYear % 100 != 0) || endYear % 400 == 0 ? 29 : 28;
+  // number of days in each month
   var daysOfMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
   var startDateNotPassedInEndYear = (endMonth < startMonth) || endMonth == startMonth && endDay < startDay;
+  // years
   var years = endYear - startYear - (startDateNotPassedInEndYear ? 1 : 0);
-
+  // months
   var months = (12 + endMonth - startMonth - (endDay < startDay ? 1 : 0)) % 12;
-
-  // (12 + ...) % 12 makes sure index is always between 0 and 11
+  // days
   var days = startDay <= endDay ? endDay - startDay : daysOfMonth[(12 + endMonth - 1) % 12] - startDay + endDay;
 
   return {
