b4d86f508ca2d34d16da87f5d7de20aec0f3295a
Matthew <krashstudios@gmail.com>
2018-12-03 17:12:41 -0700

    javascript class chapter 12
    updated for to use jquery

--- solar.php
+++
@@ -1,6 +0,0 @@
-<?php
-$WeatherSource = "https://api.forecast.io/forecast/8c59eaa35cc09f8f8e8c37940bf23e3d/" . $_GET["lat"] . "," . $_GET["lng"];
-header("Content-Type: application/json");
-header("Cache-Control: no-cache");
-readfile($WeatherSource);
-?>
